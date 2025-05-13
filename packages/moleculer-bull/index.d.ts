67bb7f06d334fb29df79ec1a8e7cfcea9c145812
Xì Gà <vandug23@gmail.com>
2025-05-13 23:05:01 +0700

    feat(bull): add index.d.ts

---
+++ packages/moleculer-bull/index.d.ts
@@ -0,0 +1,22 @@
+declare module "moleculer-bull" {
+  import { ServiceSchema } from "moleculer";
+  import { Job } from "bull";
+
+  type QueueHandler = (job: Job) => Promise<any> | any;
+
+  interface QueueSchema {
+    // completly replace the initial queueOpts
+    options?: QueueOptions;
+    name?: string;
+    concurrency?: number;
+    process: QueueHandler;
+  }
+
+  export interface BullServiceSchema extends ServiceSchema {
+    queues?: Record<string, QueueHandler | QueueSchema | QueueSchema[]>;
+  }
+
+  function createQueueService(url: string, queueOpts?: QueueOptions): BullServiceSchema;
+
+  export = createQueueService;
+}
